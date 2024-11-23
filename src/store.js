d757c579d0772ba5e4c74516f97df43853e05a1f
mahdi ezati <mahdiezati80@gmail.com>
2024-11-23 22:57:13 +0330

    Provider File.

--- src/store.js
+++ src/store.js
@@ -1,68 +1,13 @@
-import { combineReducers, createStore } from "redux";
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-const initialStateCustomer = {
-  fullName: "",
-  nationalId: "",
-  createdAt: "",
-};
-
-function accountreducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/reqestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-
-    default:
-      return state;
-  }
-}
-
-function customerreducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalId: action.payload.nationalId,
-        createdAt: action.payload.createdAt,
-      };
-    case "updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-    default:
-      return state;
-  }
-}
-
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 const rootReducer = combineReducers({
-  account: accountreducer,
-  customer: customerreducer,
+  account: accountReducer,
+  customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 // store.dispatch({ type: "account/deposit", payload: 1000 });
 // console.log(store.getState());
 // store.dispatch({
@@ -75,45 +20,4 @@ const store = createStore(rootReducer);
 // console.log(store.getState());
 // store.dispatch({ type: "account/payLoan" });
 // console.log(store.getState());
-
-function deposit(amount) {
-  return {
-    type: "account/deposit",
-    payload: amount,
-  };
-}
-
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/reqestLoan",
-    payload: {
-      amount,
-      purpose,
-    },
-  };
-}
-
-function payLoan(amount) {
-  return { type: "account/payLoan" };
-}
-
-function createCustomer(fullName, nationalId) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName,
-      nationalId,
-      createdAt: new Date().toISOString(),
-    },
-  };
-}
-
-function updateName(fullName) {
-  return {
-    type: "account/updateName",
-    payload: fullName,
-  };
-}
+export default store;
